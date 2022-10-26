9003ce6d642824e2d58f0584cdb7a8218c0d8e22
Johannes Langosch <johannes.langosch@googlemail.com>
2022-10-26 20:21:55 +0200

    05 add products display it in admin and index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import prisma from "lib/prisma";
+import { getProducts } from "lib/data.js";
+import Image from "next/image";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div>
       <Head>
@@ -10,8 +13,38 @@ export default function Home() {
       </Head>
 
       <div className="text-center text-black ">
-        <h1 className="mt-10 font-extrabold text-2xl">Shop</h1>
+        <h1 className="mt-4 font-extrabold text-2xl">Shop</h1>
+        <div className="mt-5 mx-auto max-w-sm sm:w-full sm:flex sm:flex-row">
+          {products.map((product) => (
+            <div
+              className="flex flex-col mb-4 sm:w-full rounded-2xl p-2 bg-gradient-to-tl from-green-300 to-blue-300 shadow-lg"
+              key={product.id}
+            >
+              <div className="text-center text-lg pb-1 font-semibold ">
+                {product.title} (${product.price / 100})
+              </div>
+              <Image
+                className="rounded-lg"
+                src={"/" + `${product.image}`}
+                alt={product.title}
+                width={300}
+                height={300}
+              />
+              <p className="mt-2">{product.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await getProducts(prisma);
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
